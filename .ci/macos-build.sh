e4ef699d7a370178f513faa1090bfefb73062317
Rahul Shyam <rahulshyamcv@gmail.com>
2026-09-04 11:41:08 +0530

    Fix CI: upgrade deprecated artifact actions to v4, replace retired ubuntu-20.04 runners, use prebuilt linuxdeploy, fix macOS OpenSSL detection, remove unavailable Qt deps

--- .ci/macos-build.sh
+++ .ci/macos-build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-export CPATH=$CPATH:`echo /usr/local/Cellar/openssl@1.1/1.1.1*/include`
-export LIBRARY_PATH=`echo /usr/local/Cellar/openssl@1.1/1.1.1*/lib`
+# Detect OpenSSL from Homebrew (supports openssl@3 and openssl@1.1, x86_64 and ARM paths)
+for ssl in /usr/local/opt/openssl@3 /opt/homebrew/opt/openssl@3 /usr/local/opt/openssl@1.1 /opt/homebrew/opt/openssl@1.1 /usr/local/Cellar/openssl@3 /opt/homebrew/Cellar/openssl@3; do
+	if [ -d "$ssl/include" ]; then
+		export CPATH=$CPATH:$ssl/include
+		export LIBRARY_PATH=$LIBRARY_PATH:$ssl/lib
+		echo "Using OpenSSL from: $ssl"
+		break
+	fi
+done
+
 VERSION=$(head -n 1 .qmake.conf)
 VERSION=${VERSION:8}
 VERSION_MAJOR=$(echo $VERSION | sed 's/\..*//')
--- .ci/ubuntu-build.sh
+++ .ci/ubuntu-build.sh
@@ -11,56 +11,32 @@ if [[ "$1" != "0" ]]; then
     .ci/common/build.sh linux || exit 1
 fi
 
-# Build linuxdeploy
-sudo apt install -y build-essential g++ pkg-config curl wget libpng-dev libjpeg-dev zsync desktop-file-utils libxcb-cursor0 &&
-git clone https://github.com/Open-Typer/linuxdeploy linuxdeploy-build --recurse-submodules &&
-mkdir linuxdeploy-build/build
-cd linuxdeploy-build/build &&
-# Add src/core/copyright to include paths (see https://github.com/linuxdeploy/linuxdeploy/issues/212)
-cmake -DCMAKE_CXX_FLAGS=-isystem\ $(pwd)/../src/core/copyright .. &&
-make -j$(nproc --all) &&
-mv bin/linuxdeploy ../.. &&
-cd ../.. &&
-rm -rf linuxdeploy-build &&
+# Install build dependencies
+sudo apt-get update &&
+sudo apt install -y build-essential g++ pkg-config curl wget libpng-dev libjpeg-dev zsync desktop-file-utils libxcb-cursor0 patchelf || exit 1
 
-# Build linuxdeploy-plugin-qt
-sudo apt install -y nlohmann-json3-dev &&
-git clone https://github.com/Open-Typer/linuxdeploy-plugin-qt plugin-qt --recurse-submodules &&
-mkdir plugin-qt/build &&
-cd plugin-qt/build &&
-cmake .. &&
-make -j$(nproc --all) &&
-mv bin/linuxdeploy-plugin-qt ../.. &&
-cd ../.. &&
-rm -rf plugin-qt &&
+# Download prebuilt linuxdeploy and plugins (AppImage releases)
+LD_URL="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous"
+LDQT_URL="https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous"
+LDAI_URL="https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous"
 
-# Build linuxdeploy-plugin-appimage
-git clone https://github.com/linuxdeploy/linuxdeploy-plugin-appimage plugin-appimage --recurse-submodules &&
-mkdir plugin-appimage/build &&
-cd plugin-appimage/build &&
-cmake .. &&
-make -j$(nproc --all) &&
-mv src/linuxdeploy-plugin-appimage ../.. &&
-cd ../.. &&
-rm -rf plugin-appimage &&
+case "$(arch)" in
+    aarch64)
+        LD_ARCH="aarch64"
+        ;;
+    armv7l|armv6l)
+        LD_ARCH="armhf"
+        ;;
+    *)
+        LD_ARCH="x86_64"
+        ;;
+esac
 
-# Build AppImageKit
-sudo apt install -y snapd squashfs-tools &&
-sudo snap install docker &&
-git clone https://github.com/AppImage/AppImageKit --recurse-submodules &&
-cd AppImageKit &&
-sudo env ARCH=$(arch) bash ci/build.sh
-sudo cp out/appimagetool /usr/bin/ &&
-sudo cp out/digest /usr/bin/ &&
-sudo cp out/validate /usr/bin/ &&
-cd .. &&
-sudo mkdir -p /usr/lib/appimagekit &&
-sudo ln -s /usr/bin/mksquashfs /usr/lib/appimagekit/mksquashfs &&
+curl -L -o linuxdeploy "$LD_URL/linuxdeploy-$LD_ARCH.AppImage" &&
+curl -L -o linuxdeploy-plugin-qt "$LDQT_URL/linuxdeploy-plugin-qt-$LD_ARCH.AppImage" &&
+curl -L -o linuxdeploy-plugin-appimage "$LDAI_URL/linuxdeploy-plugin-appimage-$LD_ARCH.AppImage" &&
+chmod +x linuxdeploy linuxdeploy-plugin-qt linuxdeploy-plugin-appimage || exit 1
 
-# Install patchelf from PyPI (see https://github.com/linuxdeploy/linuxdeploy-plugin-qt/issues/133#issuecomment-1608168363)
-# TODO: Remove this after dropping Ubuntu 20.04
-sudo apt install -y python3-pip
-pip3 install patchelf
 export PATH=$PATH:~/.local/bin
 
 # Build AppImage
