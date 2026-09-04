24ca800accef48687db8e8670224d2852ba216ea
Rahul Shyam <rahulshyamcv@gmail.com>
2026-09-04 11:46:04 +0530

    Fix CI: restore executable bits on .ci scripts, pin aqtinstall 3.1.6 for Qt 6.5.2, create C:/OpenSSL dir

--- .ci/windows-build.sh
+++ .ci/windows-build.sh
@@ -2,8 +2,9 @@
 
 if [[ "$1" != "1" ]]; then
 	curl -o openssl.zip -LJO https://github.com/CristiFati/Prebuilt-Binaries/raw/master/OpenSSL/v3.0/OpenSSL-3.0.7-Win-pc064.zip
-	mkdir ../openssl
+	mkdir -p ../openssl
 	unzip openssl.zip -d ../openssl
+	mkdir -p C:/OpenSSL
 	mv ../openssl/OpenSSL/OpenSSL/* C:/OpenSSL
 	rm openssl.zip
 	find C:/OpenSSL/lib -depth -name "*.lib" -exec sh -c 'f="{}"; mv -- "$f" "${f%.lib}.a"' \;
